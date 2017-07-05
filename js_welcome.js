50543b391cba4bdde98e6a49970b3058543fa3ca
RaiManish3 <manish.07.1996@gmail.com>
2017-07-05 05:45:31 +0530

    personal webpage basics done.

---
+++ js_welcome.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	    $('#hasAbtMe').addClass('animatable');
+	        setTimeout(function () {
+			        $('#hasAbtMe').removeClass('animatable');
+				    }, 1000)
+});
