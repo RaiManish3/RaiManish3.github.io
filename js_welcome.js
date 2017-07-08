7ac947fa772c31a3a06d72314a5e3b6965941d79
RaiManish3 <manish.07.1996@gmail.com>
2017-07-09 04:28:24 +0530

    works update

--- js_welcome.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-	    $('#hasAbtMe').addClass('animatable');
-	        setTimeout(function () {
-			        $('#hasAbtMe').removeClass('animatable');
-				    }, 1000)
-});
